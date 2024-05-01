7e3ef511e9dd86366685ec9c750a7153781aa133
ermolaevv <ermolaevvladislav1485@gmail.com>
2024-05-01 10:02:29 +0300

    upd

--- IoT/backend/notes/models.py
+++ IoT/backend/notes/models.py
@@ -6,3 +6,10 @@ class Telemetry(models.Model):
     token = models.CharField(max_length=128)
     ressived_time = models.DateTimeField(default=lambda: datetime.now().isoformat())
     data = models.JSONField()
+
+class Devices(models.Model):
+    model = models.CharField(max_length=100)
+    serial_number = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    owner = models.IntegerField()
+    token = models.CharField(max_length=128)
--- IoT/backend/notes/views.py
+++ IoT/backend/notes/views.py
@@ -2,24 +2,37 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Telemetry
+from .models import Telemetry, Devices
 from django.forms.models import model_to_dict
 from django.utils import timezone
+from django.core.exceptions import ObjectDoesNotExist
 
 @api_view(['GET', 'POST'])
 def telemetry(request):
+    url = request.get_full_path().split("?")[1]
+    params = dict()
+
+    for param in url.split("&"):
+        name, value = param.split("=")
+        params[name] = value
+
+
     if request.method == 'GET':
         print("GET", request.data)
-        return Response(request.data, status=status.HTTP_200_OK)
+
+        try:
+            telemetry = Telemetry.objects.filter(token=params['token']).latest('ressived_time')
+        except ObjectDoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        return Response(model_to_dict(telemetry), status=status.HTTP_200_OK)
 
     elif request.method == 'POST':
         print("POST", request.data)
-        url = request.get_full_path().split("?")[1]
-        params = dict()
-
-        for param in url.split("&"):
-            name, value = param.split("=")
-            params[name] = value
+        
+        try:
+            Devices.objects.get(token=params['token'])
+        except ObjectDoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
         telemetry = Telemetry.objects.create(
             token= params['token'],
@@ -28,6 +41,7 @@ def telemetry(request):
         )
         
         return Response(model_to_dict(telemetry), status=status.HTTP_200_OK)
+    return Response(status=status.HTTP_418_IM_A_TEAPOT)
 
 def index(request):
     return render(request,'index.html')
